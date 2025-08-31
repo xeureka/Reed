4780222d35e616996c6ddf790962f6682c4ba68d
xeureka <teklemariameureka@gmail.com>
2025-08-31 08:42:52 +0300

    render dempyment setup

--- backend/index.ts
+++ backend/index.ts
@@ -15,3 +15,6 @@ const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`server running at port ${PORT}`)
 })
+
+// depoy the backend
+// thinking about the biggest system design and workin on it
